ec96285c04455d5681978640c735e54b46c805ba
Andres A <andres_pikpok@hotmail.com>
2022-04-03 00:14:51 -0600

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./src/assets/scripts/index.js'],
+  entry: ['./src/assets/scripts/index.js', './src/assets/js/theme.js'],
   output: {
     path: path.resolve(__dirname, 'src/assets/scripts'),
     filename: 'bundle.min.js'
